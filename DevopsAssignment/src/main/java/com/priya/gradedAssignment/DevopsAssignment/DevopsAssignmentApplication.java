af70541ae6eadc3f34666c65df7aabebb1d8bcaa
student <chappidi.keert@hcl.com>
2021-04-20 11:25:12 +0000

    First Commit

---
+++ DevopsAssignment/src/main/java/com/priya/gradedAssignment/DevopsAssignment/DevopsAssignmentApplication.java
@@ -0,0 +1,14 @@
+package com.priya.gradedAssignment.DevopsAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsAssignmentApplication.class, args);
+		System.out.println("Hello Dev-Ops");
+	}
+
+}
---
+++ DevopsAssignment/src/test/java/com/priya/gradedAssignment/DevopsAssignment/DevopsAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priya.gradedAssignment.DevopsAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
